b453162bbcb876e9b24a919a3593807b4b7f9fdc
AlanYang99 <33969636+AlanYang99@users.noreply.github.com>
2017-11-28 11:50:42 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+function deletePost() {
+    var userinput = document.getElementById("comment").value; 
 
+    if(userinput == "") {
+        
+        alert("Please write a comment!");
+    } else if (userinput == "Comment...") {
+        alert("Please write a comment!");    
+    } else {
+        console.log(userinput);  
+        var para = document.createElement("p");
+        var node = document.createTextNode(userinput);
+        para.appendChild(node);
+        var element = document.getElementById("newsfeed");  
+        element.appendChild(para);  
+        document.getElementById("frm2").reset();
+    }
+}
